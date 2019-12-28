2ca5b2c9a72808465c4fc7704c9f5b14951a76fb
Rodolfo Farinha <rodolfo.alves.farinha@gmail.com>
2019-12-28 13:30:11 +0000

    SearchServices

--- pt.iscte.pidesco.search/src/pt/iscte/pidesco/search/internal/Search.java
+++ pt.iscte.pidesco.search/src/pt/iscte/pidesco/search/internal/Search.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
@@ -12,11 +13,12 @@ import org.osgi.framework.ServiceReference;
 
 import pt.iscte.pidesco.javaeditor.service.JavaEditorServices;
 import pt.iscte.pidesco.projectbrowser.service.ProjectBrowserServices;
+import pt.iscte.pidesco.search.service.SearchServices;
 import pt.iscte.pidesco.projectbrowser.model.ClassElement;
 import pt.iscte.pidesco.projectbrowser.model.PackageElement;
 import pt.iscte.pidesco.projectbrowser.model.PackageElement.Visitor;
 
-public class Search {
+public class Search implements SearchServices{
 	
 	private BundleContext context = Activator.getContext();
 	
@@ -25,8 +27,7 @@ public class Search {
 	
 	private PackageElement packageElement = projectBrowserServices.getRootPackage();	
 	
-	public void search(boolean [] conditions, String word, String packageItem) {
-		
+	public void search(boolean [] conditions, String word, String packageItem) {		
 		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
 		packageElement.traverse(searchVisitor);
 		
@@ -34,10 +35,239 @@ public class Search {
 	}
 	
 	public void getPackage() {
-
 		PackageVisitor packageVisitor = new PackageVisitor();	
 		packageElement.traverse(packageVisitor);
 		
 		SearchView.addItemsCombo(packageVisitor.packageClass);
+	}
+	
+	@Override
+	public void searchAllFileEquals(String word, String packageItem) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 	// caseSensitive
+		conditions[1] = true;	// equals
+		conditions[2] = false;	// contains
+		conditions[3] = false;	// startsWith
+		conditions[4] = false;	// endsWith
+		conditions[5] = true;	// allFiles
+		conditions[6] = false;	// types
+		conditions[7] = false;	// methods
+		conditions[8] = false;	// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchAllFileEqualsCaseSensitive(String word, String packageItem) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = true; 	// caseSensitive
+		conditions[1] = true;	// equals
+		conditions[2] = false;	// contains
+		conditions[3] = false;	// startsWith
+		conditions[4] = false;	// endsWith
+		conditions[5] = true;	// allFiles
+		conditions[6] = false;	// types
+		conditions[7] = false;	// methods
+		conditions[8] = false;	// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchAllFileContains(String word, String packageItem) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 	// caseSensitive
+		conditions[1] = false;	// equals
+		conditions[2] = true;	// contains
+		conditions[3] = false;	// startsWith
+		conditions[4] = false;	// endsWith
+		conditions[5] = true;	// allFiles
+		conditions[6] = false;	// types
+		conditions[7] = false;	// methods
+		conditions[8] = false;	// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchAllFileStartsWith(String word, String packageItem) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 	// caseSensitive
+		conditions[1] = false;	// equals
+		conditions[2] = false;	// contains
+		conditions[3] = true;	// startsWith
+		conditions[4] = false;	// endsWith
+		conditions[5] = true;	// allFiles
+		conditions[6] = false;	// types
+		conditions[7] = false;	// methods
+		conditions[8] = false;	// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchAllFileEndsWith(String word, String packageItem) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 	// caseSensitive
+		conditions[1] = false;	// equals
+		conditions[2] = false;	// contains
+		conditions[3] = false;	// startsWith
+		conditions[4] = true;	// endsWith
+		conditions[5] = true;	// allFiles
+		conditions[6] = false;	// types
+		conditions[7] = false;	// methods
+		conditions[8] = false;	// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchEquals(String word, String packageItem, boolean types, boolean methods, boolean fields) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 		// caseSensitive
+		conditions[1] = true;		// equals
+		conditions[2] = false;		// contains
+		conditions[3] = false;		// startsWith
+		conditions[4] = false;		// endsWith
+		conditions[5] = false;		// allFiles
+		conditions[6] = types;		// types
+		conditions[7] = methods;	// methods
+		conditions[8] = fields;		// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchEqualsCaseSensitive(String word, String packageItem, boolean types, boolean methods, boolean fields) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = true; 		// caseSensitive
+		conditions[1] = true;		// equals
+		conditions[2] = false;		// contains
+		conditions[3] = false;		// startsWith
+		conditions[4] = false;		// endsWith
+		conditions[5] = false;		// allFiles
+		conditions[6] = types;		// types
+		conditions[7] = methods;	// methods
+		conditions[8] = fields;		// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchContains(String word, String packageItem, boolean types, boolean methods, boolean fields) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 		// caseSensitive
+		conditions[1] = false;		// equals
+		conditions[2] = true;		// contains
+		conditions[3] = false;		// startsWith
+		conditions[4] = false;		// endsWith
+		conditions[5] = false;		// allFiles
+		conditions[6] = types;		// types
+		conditions[7] = methods;	// methods
+		conditions[8] = fields;		// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchStartsWith(String word, String packageItem, boolean types, boolean methods, boolean fields) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 		// caseSensitive
+		conditions[1] = false;		// equals
+		conditions[2] = false;		// contains
+		conditions[3] = true;		// startsWith
+		conditions[4] = false;		// endsWith
+		conditions[5] = false;		// allFiles
+		conditions[6] = types;		// types
+		conditions[7] = methods;	// methods
+		conditions[8] = fields;		// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
+	}
+	
+	@Override
+	public void searchEndsWith(String word, String packageItem, boolean types, boolean methods, boolean fields) {
+		Assert.isNotNull(word, "word cannot be null");
+		Assert.isNotNull(packageItem, "packageItem cannot be null");
+		
+		boolean[] conditions = new boolean[9];
+		
+		conditions[0] = false; 		// caseSensitive
+		conditions[1] = false;		// equals
+		conditions[2] = false;		// contains
+		conditions[3] = false;		// startsWith
+		conditions[4] = true;		// endsWith
+		conditions[5] = false;		// allFiles
+		conditions[6] = types;		// types
+		conditions[7] = methods;	// methods
+		conditions[8] = fields;		// fields
+		
+		SearchVisitor searchVisitor = new SearchVisitor(conditions, word, packageItem);	
+		packageElement.traverse(searchVisitor);
+		
+		SearchView.showTree(searchVisitor.pathClass, searchVisitor.packageClass, searchVisitor.types, searchVisitor.fileLines, searchVisitor.methods, searchVisitor.fields);
 	}
 }
--- pt.iscte.pidesco.search/src/pt/iscte/pidesco/search/service/SearchServices.java
+++ pt.iscte.pidesco.search/src/pt/iscte/pidesco/search/service/SearchServices.java
@@ -1,6 +1,99 @@
 package pt.iscte.pidesco.search.service;
 
+import java.io.File;
+
+/**
+ * Services offered by the Search component.
+ */
 public interface SearchServices {
+	
+	/**
+	 * Search if equals the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select 
+	 * if packageItem is empty, app searchs in all classes
+	 */
+	void searchAllFileEquals(String word, String packageItem);
+	
+	/**
+	 * Search if equals and case sensitive the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select 
+	 * if packageItem is empty, app searchs in all classes
+	 */
+	void searchAllFileEqualsCaseSensitive(String word, String packageItem);
+	
+	/**
+	 * Search if contains the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select 
+	 * if packageItem is empty, app searchs in all classes
+	 */
+	void searchAllFileContains(String word, String packageItem);
+	
+	/**
+	 * Search if starts with the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select 
+	 * if packageItem is empty, app searchs in all classes
+	 */
+	void searchAllFileStartsWith(String word, String packageItem);
+	
+	/**
+	 * Search if ends with the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select 
+	 * if packageItem is empty, app searchs in all classes
+	 */
+	void searchAllFileEndsWith(String word, String packageItem);
 
-
+	/**
+	 * Search if equals the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select (if packageItem is empty, app searchs in all classes)
+	 * @param types if true just search the word in types declarations
+	 * @param methods if true just search the word in methods declarations
+	 * @param fields if true just search the word in fields declarations
+	 */
+	void searchEquals(String word, String packageItem, boolean types, boolean methods, boolean fields);
+	
+	/**
+	 * Search if equals and case sensitive the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select (if packageItem is empty, app searchs in all classes)
+	 * @param types if true just search the word in types declarations
+	 * @param methods if true just search the word in methods declarations
+	 * @param fields if true just search the word in fields declarations
+	 */
+	void searchEqualsCaseSensitive(String word, String packageItem, boolean types, boolean methods, boolean fields);
+	
+	/**
+	 * Search if contains the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select (if packageItem is empty, app searchs in all classes)
+	 * @param types if true just search the word in types declarations
+	 * @param methods if true just search the word in methods declarations
+	 * @param fields if true just search the word in fields declarations
+	 */
+	void searchContains(String word, String packageItem, boolean types, boolean methods, boolean fields);
+	
+	/**
+	 * Search if starts with the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select (if packageItem is empty, app searchs in all classes)
+	 * @param types if true just search the word in types declarations
+	 * @param methods if true just search the word in methods declarations
+	 * @param fields if true just search the word in fields declarations
+	 */
+	void searchStartsWith(String word, String packageItem, boolean types, boolean methods, boolean fields);
+	
+	/**
+	 * Search if ends with the word in all lines from all classes.
+	 * @param word (non-null) word to search
+	 * @param packageItem (non-null) package to search the word select (if packageItem is empty, app searchs in all classes)
+	 * @param types if true just search the word in types declarations
+	 * @param methods if true just search the word in methods declarations
+	 * @param fields if true just search the word in fields declarations
+	 */
+	void searchEndsWith(String word, String packageItem, boolean types, boolean methods, boolean fields);
 }
